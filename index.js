ac7412e63a85dabeeba0a8f0cb8c2f0c43b649a9
Thomas Frank <thomasfrank@Thomass-MacBook-2.local>
2019-11-09 19:19:29 +0100

    changing to working cwd

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 const fs = require('fs');
 const { execSync } = require('child_process');
 const sleep = (ms => new Promise(res => setTimeout(res, 1000)));
-const silent = {stdio : 'pipe' };
 
 // execSync executes shell commands
 // & in a shell command - execute several commands after each other
 // & : - end with a null command those not throwing errors
 // back in Node.js even if there is an error in the shell command
-
-
-// The option stored in silent prevents a call to execSynt
-// to show the result in the console
 
 async function doIt() {
   while (1) {
@@ -20,9 +15,12 @@ async function doIt() {
       watch = JSON.parse(fs.readFileSync('./watch.json', 'utf-8'));
     } catch (e) { }
     for (repo of watch) {
+      // execute silently in a certain folder
+      let options =  {stdio : 'pipe', cwd: repo.repoPath}
+      execSync(`git fetch & :`, options); 
       // do two pulls in a row
-      let a = execSync(`cd "${repo.repoPath}" & git pull & :`, silent).toString();
-      let b = execSync(`cd "${repo.repoPath}" & git pull & :`, silent).toString();
+      let a = execSync(`cd "${repo.repoPath}" & git pull & :`, options).toString();
+      let b = execSync(`cd "${repo.repoPath}" & git pull & :`, options).toString();
       if(a !== b){
         // something updated between pulls so restart app
         execSync(`pm2 restart "${repo.pm2Name}" & :`, silent);

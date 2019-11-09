bace0625303546cdbdff09ebc223674e81d8a16d
Thomas Frank <thomasfrank@Thomass-MacBook-2.local>
2019-11-09 19:28:48 +0100

    added npm install

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const sleep = (ms => new Promise(res => setTimeout(res, 1000)));
 
 // execSync executes shell commands
 // & in a shell command - execute several commands after each other
-// & : - end with a null command those not throwing errors
+// & : - end with a null command, thus not throwing errors
 // back in Node.js even if there is an error in the shell command
 
 async function doIt() {
@@ -22,7 +22,9 @@ async function doIt() {
       let a = execSync(`cd "${repo.repoPath}" & git pull & :`, options).toString();
       let b = execSync(`cd "${repo.repoPath}" & git pull & :`, options).toString();
       if(a !== b){
-        // something updated between pulls so restart app
+        // something updated between pulls so do npm install
+        // and then restart app
+        execSync(`npm install & :`, options);
         execSync(`pm2 restart "${repo.pm2Name}" & :`, options);
         console.log(`restarting ${repo.pm2Name}`)
       }
